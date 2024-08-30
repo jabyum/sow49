19d3d258a3b266e9d578f08de8fcddf820af673a
jabyum <jabyum@github.com>
2024-08-30 14:20:33 +0500

    asd

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from users import user_bp
 from questions import question_bp
 app = Flask(__name__)
@@ -13,5 +13,8 @@ def home():
     user_link = "<a href='/user/'> Юзеры </a><br>"
     question_link = "<a href='/question/'> Вопросы </a><br>"
     return user_link + question_link
+@app.route("/reg", methods=["POST"])
+def reg():
+    print(request.form)
 # запуск
 app.run(debug=True)
